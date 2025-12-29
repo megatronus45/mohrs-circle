bfaef2adddf0470ae28931083fec42aadc6ce50f
blobby45 <nsyed5192@gmail.com>
2025-12-29 02:58:30 -0500

    Final revised

--- app.py
+++ app.py
@@ -1,5 +1,127 @@
 import streamlit as st
-from main import inputs
+import numpy as np
+import io
+from main import eqns, slider, o_plot
 
-st.write("hello world")
-s_x, s_y, t_xy = inputs()
+st.title("\tMy Stress-Analysis Tool!")
+st.write("**Assumptions: _plane stress conditions, counterclockwise is positive, units of Pascals (MPa, GPa, etc..)_**")
+
+if 'button_clicked' not in st.session_state:
+        st.session_state.button_clicked = False
+
+if 'theta_slider' not in st.session_state:
+        st.session_state.theta_slider = 0.0
+
+st.divider()
+
+cols = st.columns([1, 1, 2])
+
+with cols[0]:
+
+    s_x = st.number_input("Enter σₓ:", 
+                          min_value=-1e15,
+                          max_value=1e15,
+                          value = 0.0,
+                          step=2.0,
+                          help="Normal stress in the x-direction.")
+    
+    s_y = st.number_input("Enter σᵧ:", 
+                          min_value=-1e15,
+                          max_value=1e15,
+                          value = 0.0,
+                          step=2.0,
+                          help="Normal stress in the y-direction.")
+    
+    t_xy = st.number_input("Enter τₓᵧ:", 
+                           min_value=-1e15,
+                           max_value=1e15,
+                           value = 0.0,
+                           step=2.0,
+                           help="Shear stress in the xy-plane.")
+
+with cols[2]:
+    
+    theta_slider = 0
+    theta_p = np.degrees(0.5 * np.arctan2(2 * t_xy, s_x - s_y))
+
+    if st.button(
+        label = "**Mohrs Circle**", 
+        use_container_width = True):
+
+        st.session_state.show_circle = True
+
+    if "show_circle" not in st.session_state:
+        st.session_state.show_circle = False
+
+
+    if st.session_state.show_circle:
+        st.session_state.theta_slider = np.radians(
+            st.slider(
+                "Angle (θ°)", 
+                min_value= 0.0, 
+                max_value=180.0, 
+                value= abs(theta_p - 90), 
+                step=0.1
+            ) 
+        )
+
+        theta_slider = st.session_state.theta_slider
+
+        r, s_avg, s_min, s_max, s_x, s_y, t_xy = eqns(s_x, s_y, t_xy)
+        circle_x, circle_y, sx_prime, sy_prime, t_prime, theta_slider = slider(r, s_avg, theta_slider, s_x, s_y, t_xy)
+        fig = o_plot(r, s_avg, s_max, s_min, s_x, s_y, t_xy, circle_x, circle_y, sx_prime, sy_prime, t_prime)
+
+        st.pyplot(fig)
+
+        buf = io.BytesIO()
+        fig.savefig(buf, format="png")
+        buf.seek(0)
+
+        st.download_button(
+            label="Download Mohr's Circle as PNG",
+            data = buf,
+            file_name="mohrs_circle.png",
+            mime = "image/png"
+        )
+        
+   
+    
+with cols[1]:
+
+    r, s_avg, s_min, s_max, s_x, s_y, t_xy = eqns(s_x, s_y, t_xy)
+    circle_x, circle_y, sx_prime, sy_prime, t_prime, theta_slider = slider(r, s_avg, theta_slider, s_x, s_y, t_xy)
+    fig = o_plot(r, s_avg, s_max, s_min, s_x, s_y, t_xy, circle_x, circle_y, sx_prime, sy_prime, t_prime)
+    
+    st.text_input("Max Stress",
+                    value = f"{s_max:.2f}",
+                    help = "Stress is max and min when shear is 0",
+                    disabled = True)
+    
+    st.text_input("Min Stress",
+                    value = f"{s_min:.2f}",
+                    help = "Stress is min when shear is 0",
+                    disabled = True)
+    
+    st.text_input("Max Shear Stress",
+                    value = f"{r:.2f}",
+                    help = "Max shear stress is the radius of the Mohr's Circle",
+                    disabled = True)
+    
+    st.text_input("Shear (τ') at angle θ",
+                    value = f"{t_prime:.2f}",      
+                    help = "Shear when the principal angle is changed",
+                    disabled = True)
+    
+    r, s_avg, s_min, s_max, s_x, s_y, t_xy = eqns(s_x, s_y, t_xy) 
+    circle_x, circle_y, sx_prime, sy_prime, t_prime, theta_slider = slider(r, s_avg, theta_slider, s_x, s_y, t_xy)
+
+    st.text_input("Stress (σₓ') at angle θ",
+                    value = f"{sx_prime:.2f}",     
+                    help = "Stress in x when the principal angle is changed",
+                    disabled = True)
+    
+    st.text_input("Stress (σᵧ') at angle θ",
+                    value = f"{sy_prime:.2f}",     
+                    help = "Stress in y when the principal angle is changed",
+                    disabled = True)
+    
--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
- #   s_x = float(input("Enter σx (MPa):"))
-  #  s_y = float(input("Enter σy (MPa):"))
-   # t_xy = float(input("Enter τxy (MPa):"))
-
-
 def eqns(s_x, s_y, t_xy):
     
     r = t_max = np.sqrt(((s_x - s_y)/2)**2 + (t_xy)**2)
@@ -14,41 +8,43 @@ def eqns(s_x, s_y, t_xy):
     s_max = s_avg + t_max
     s_min = s_avg - t_max
 
-    return r, s_avg, s_min, s_max
+    return r, s_avg, s_min, s_max, s_x, s_y, t_xy
+    
+def slider(r, s_avg, theta_slider, s_x, s_y, t_xy):
 
-def circle(s_avg, r):
-    phi = np.linspace(0, 2 * np.pi, 500)
+    phi = np.linspace(0, 2*np.pi, 500)
 
-    s_prime = s_avg + r * np.cos(phi)
-    t_prime = -r * np.sin(phi)
+    theta_p = 0.5 * np.arctan2(2 * t_xy, s_x - s_y)
+    mohr_angle = 2 * (theta_slider + theta_p)
 
-    circle_x = s_prime
-    circle_y = t_prime
+    sx_prime = s_avg + ((s_x - s_y)/2) * np.cos(mohr_angle) + t_xy * np.sin(mohr_angle)
+    sy_prime = s_avg - ((s_x - s_y)/2) * np.cos(mohr_angle) - t_xy * np.sin(mohr_angle)
+    t_prime = -((s_x - s_y)/2) * np.sin(mohr_angle) + t_xy * np.cos(mohr_angle)
 
-    return circle_x, circle_y
+    circle_x = s_avg + r * np.cos(phi)
+    circle_y = r * np.sin(phi)
 
-def angles(t_xy, s_x, s_y):
-    theta_p = 0.5 * np.arctan2(2*t_xy, (s_x - s_y)) 
-    theta_s = theta_p + np.pi/4
+    return circle_x, circle_y, sx_prime, sy_prime, t_prime, theta_slider
 
-    theta_p = np.degrees(theta_p)
-    theta_s = np.degrees(theta_s)
-
-    return theta_p, theta_s
-
-def o_plot(x, y, xkeypts, ykeypts, circle_x, circle_y, r, s_avg, s_max, s_min, s_x, s_y, t_xy):
+def o_plot(r, s_avg, s_max, s_min, s_x, s_y, t_xy, circle_x, circle_y, sx_prime, sy_prime, t_prime):
 
     x = np.array([s_x, s_y])
     y = np.array([t_xy, -t_xy])
     xkeypts = np.array([s_avg, s_max, s_min, s_avg])
     ykeypts = np.array([0, 0, 0, r])
 
+    s_primept = np.array([sx_prime, sy_prime])
+    t_primept = np.array([t_prime, -t_prime])
+
+
+
+    fig, ax = plt.subplots()
+
     plt.title(
             label = 'Mohrs Stress Circle', 
             loc = 'center'
             )
-    plt.xlabel("σ (MPa)")
-    plt.ylabel("τ (MPa)")
+   
     plt.axis('equal')
 
     plt.axvline(
@@ -65,12 +61,12 @@ def o_plot(x, y, xkeypts, ykeypts, circle_x, circle_y, r, s_avg, s_max, s_min, s
                 linewidth = 1.5
                 )
 
-    plt.xlabel("σ (MPa)")
-    plt.ylabel("τ (MPa)")
+    plt.xlabel("σ (Normal stress)")
+    plt.ylabel("τ (Shear stress)")
 
     plt.plot(circle_x, circle_y, color = '#880808')
-
     plt.plot(x, y, '.-', color = 'black', ms = 8)
+    plt.plot(s_primept, t_primept, '.-', color = "#002F57", ms = 8)
     plt.plot(xkeypts, ykeypts, '.', color = '#880808', ms = 8)
 
-
+    return fig
